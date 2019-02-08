0e29c61d2d99a69ac97aabb9e57c442d84878547
Ariel <ingenieroariel@gmail.com>
2019-02-08 18:18:44 -0500

    Allright, now I know how to build js apps with nix - yay!

--- pkgs/kepler/default.nix
+++ pkgs/kepler/default.nix
@@ -2,6 +2,17 @@
 
 yarn2nix.mkYarnPackage {
   name = "kepler";
+
+  preInstall = ''
+    yarn build:umd
+  '';
+
+  postInstall = ''
+    cp $out/node_modules/kepler.gl/umd/keplergl.min.js $out/
+    rm -rf $out/node_modules
+    rm -rf $out/bin
+  '';
+
   src = fetchgit {
     rev = "5620599c05672c1d04d256679d0ff969aa6d77bb";
     url = "https://github.com/uber/kepler.gl";
