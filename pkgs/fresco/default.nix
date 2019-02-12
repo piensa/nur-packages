18d632a6595841b5e609596b3c692a1461750f2d
Your Name <you@example.com>
2019-02-12 17:57:39 -0500

    Fresco now compiles

--- pkgs/fresco/default.nix
+++ pkgs/fresco/default.nix
@@ -3,20 +3,22 @@
 yarn2nix.mkYarnPackage {
   name = "fresco";
 
+
   preInstall = ''
     yarn build
   '';
 
   postInstall = ''
-    cp $out/node_modules/fresco/* $out/
+    ls $out/node_modules/fresco-app/build
+    cp -r $out/node_modules/fresco-app/build/* $out/
     rm -rf $out/node_modules
     rm -rf $out/bin
   '';
 
   src = fetchgit {
-    rev = "5bc1171";
+    rev = "5b0a30d";
     url = "https://github.com/piensa/fresco";
-    sha256 = "0kqs3f7fy1p48zj68i7wfid2ik3grpm5y2fwkxq7yy00ykfm05ig";
+    sha256 = "0p0fk5r5813hhsy2cyiz584yzdmwwda56a04apz9h3i88yhqi4wd";
   };
 
 }
