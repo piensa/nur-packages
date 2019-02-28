9c0daa5f097be68d4c904858cf3bfff79818351d
Ariel <ingenieroariel@gmail.com>
2019-02-28 10:02:23 -0500

    Consentido is now logico

--- pkgs/logico/default.nix
+++ pkgs/logico/default.nix
@@ -1,15 +1,15 @@
 { stdenv, buildGoPackage, fetchgit, fetchhg, fetchbzr, fetchsvn }:
 
 buildGoPackage rec {
-  name = "consentido-unstable-${version}";
+  name = "logico-unstable-${version}";
   version = "2019-02-27";
   rev = "8054399b7151331be45e4ece2df0d94e09f4f416";
 
-  goPackagePath = "github.com/piensa/consentido";
+  goPackagePath = "github.com/piensa/logico";
 
   src = fetchgit {
     inherit rev;
-    url = "https://github.com/piensa/consentido";
+    url = "https://github.com/piensa/logico";
     sha256 = "0qvqpmz751yb3msazxnmnpzqshf35zmbasydxphii0hlwa115jab";
   };
 
