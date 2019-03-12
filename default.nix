1c07ebfb226f9bb8b33d326b3efe1c8ddb337c9a
Ariel Nunez <ingenieroariel@gmail.com>
2019-03-12 17:54:17 -0500

    Enable puertico-osm, colombia and logico

--- default.nix
+++ default.nix
@@ -7,14 +7,14 @@ rec {
   hydra = pkgs.callPackage ./pkgs/hydra { };
   keto = pkgs.callPackage ./pkgs/keto { };
   oathkeeper = pkgs.callPackage ./pkgs/oathkeeper { };
-#  logico = pkgs.callPackage ./pkgs/logico { };
+  logico = pkgs.callPackage ./pkgs/logico { };
 #  packr = pkgs.callPackage ./pkgs/packr { };
   imposm = pkgs.callPackage ./pkgs/imposm { };
   blender = pkgs.callPackage ./pkgs/blender { };
   kepler = pkgs.callPackage ./pkgs/kepler { };
   fresco = pkgs.callPackage ./pkgs/fresco { };
-#  colombia = pkgs.callPackage ./pkgs/colombia { };
-#  puertico-osm = pkgs.callPackage ./pkgs/puertico-osm { };
+  colombia = pkgs.callPackage ./pkgs/colombia { };
+  puertico-osm = pkgs.callPackage ./pkgs/puertico-osm { };
 #  jamaica = pkgs.callPackage ./pkgs/jamaica { };
 #  modules = import ./modules;
 } 
