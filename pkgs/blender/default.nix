cae4370367d5695ef570077bc1d5e7ae314bffa0
Ariel Núñez <ingenieroariel@gmail.com>
2019-08-01 16:07:26 -0500

    Update default.nix

--- pkgs/blender/default.nix
+++ pkgs/blender/default.nix
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
     url = "https://git.blender.org/blender.git";
     deepClone = true;
     rev = "v2.80";
-    sha256 = "1yrb38kbr7kifi0xizs2ccfbq43hdr061harkcyfncx8cx20bqcl";
+    sha256 = "1qnivgzvq12hi42c4ysyvlddik9lwbvw31pwb8mjkjas0bw8akf0";
   };
 
   buildInputs =
