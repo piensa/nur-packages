1cd5f5928ed2a643e10d065adff0822371cbf4bc
Ariel <ingenieroariel@gmail.com>
2019-09-03 19:32:06 -0500

    Pinned caddy2 to known working version

--- pkgs/caddy2/default.nix
+++ pkgs/caddy2/default.nix
@@ -3,18 +3,17 @@ let
   buildGoPackage12 = buildGoPackage.override { go=go_1_12; };
 in buildGoPackage12 rec {
   name = "caddy-${version}";
-  version = "2";
+  version = "2-${revision}";
+  revision = "2dc4fcc62b";
   goPackagePath = "github.com/caddyserver/caddy";
   goDeps = ./deps.nix;
   subPackages = [ "cmd/caddy" ];
-  patches = [ ./fiximport.patch ];
   src = fetchFromGitHub {
     owner = "caddyserver";
     repo = "caddy";
-    rev = "v${version}";
-    sha256 = "12a8pnjhndhkbpkfm8nn61c0nxxn16dpcihsymcvax2jy5j2sq61";
+    rev = "${revision}";
+    sha256 = "1lrvcmgpis1z0cvpkzp0wnrk1mk42h0cqgsgxbh61qbwbx5b24m1";
   };
-  modSha256 = "0h2r1jivcar924vv0lyablmjxzzarhkb8sjq8zi59dp34hcwpr08";
 
   buildFlagsArray = ''
     -ldflags=
